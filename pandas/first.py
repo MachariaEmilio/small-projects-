d679d04365da9e8b424eaf795afd0526f5f4f62e
unknown <machariaemilio68@gmail.com>
2024-08-03 11:54:23 +0300

    update

--- pandas/first.py
+++ pandas/first.py
@@ -1,9 +1,13 @@
 import pandas as pd
+
 # this restricts the data to only show the 6rows .3 head and 3 column
 pd.options.display.max_rows = 6
 
 # this reads the file and stores the data in df variable
 df = pd.read_csv('data.csv')
+# removes empty cells 
+df.dropna(inplace = True)
+print(df.to_string())
 # display all the data
 print(df)
 # displays the first 10
