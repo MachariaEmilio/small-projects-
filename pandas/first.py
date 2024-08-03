813d02f8e54fdae5adaa43226f2a54ba9a0b7a8c
unknown <machariaemilio68@gmail.com>
2024-08-03 10:13:37 +0300

    pandas project

---
+++ pandas/first.py
@@ -0,0 +1,14 @@
+import pandas as pd
+# this restricts the data to only show the 6rows .3 head and 3 column
+pd.options.display.max_rows = 6
+
+# this reads the file and stores the data in df variable
+df = pd.read_csv('data.csv')
+# display all the data
+print(df)
+# displays the first 10
+print(df.head(10))
+# displays the last 10
+print(df.tail(10))
+# displays the info of the data
+print(df.info())
